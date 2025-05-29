fccb4eaaef055f38cf6c50435af5d45407005275
techEdge3030 <atlas3030wei@gmail.com>
2025-05-30 00:52:53 +0700

    refined with composition api

--- src/views/Invoices.vue
+++ src/views/Invoices.vue
@@ -1,5 +1,16 @@
 <template>
-   <div>
-      <h1>Invoices</h1>
-   </div>
+  <div>
+    <h1>Invoices</h1>
+  </div>
 </template>
+
+<script setup>
+import { onBeforeRouteLeave } from "vue-router";
+
+onBeforeRouteLeave((to, from) => {
+  const answer = window.confirm("Are you sure you want to leave? 🥺");
+  if (!answer) {
+    return false;
+  }
+});
+</script>
